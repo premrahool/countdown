dded5f071984a4a38ddcb40fecc285f6bb0ace61
Prem Rahool Yadav <81644718+premrahool@users.noreply.github.com>
2023-01-23 11:37:34 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const endDate = "27 July 2023 08:20:00 PM"
+const endDate = "10 february2023 08:20:00 PM"
 
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
